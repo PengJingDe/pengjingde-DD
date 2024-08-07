94e135bb6d1f71aa9c8eef25ed336f87e053e5ea
pengjingde <2801514797@qq.com>
2024-08-07 21:27:03 +0800

    home

---
+++ js/translate.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var bodyElement = document.body;
+    // 这里进行 bodyElement 的操作
+    var bodyElement = document.body;
+    // 添加点击事件监听器
+    bodyElement.addEventListener('click', function() {
+       
+    // 在这里可以添加处理点击事件的代码
+    window.location.href ="other/myperson.html";
+    });
+});
